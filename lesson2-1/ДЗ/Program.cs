f0f3445ca8e5412143605ce96fcd27dc3226af1a
Natalja Veshcheva <rod2vek@gmail.com>
2023-12-23 20:15:05 +0300

    home_work_1

---
+++ lesson2-1/ДЗ/Program.cs
@@ -0,0 +1,18 @@
+﻿int firstNumber = 5;
+int secondNumber = 20;
+
+if (firstNumber == secondNumber)
+{
+    Console.WriteLine("числа равны");
+}
+else
+{
+    if (firstNumber < secondNumber)
+    {
+        Console.Write("первое число меньше второго");
+    }
+    else
+    {
+        Console.Write("первое число больше второго");
+    }
+}

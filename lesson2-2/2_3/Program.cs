c072e94fbb43648e448d879af4452c8d7e484e6f
Natalja Veshcheva <rod2vek@gmail.com>
2023-12-24 10:40:18 +0300

    2_3 2_4

---
+++ lesson2-2/2_3/Program.cs
@@ -0,0 +1,4 @@
+﻿int a = int.Parse(Console.ReadLine()!); 
+int b = int.Parse(Console.ReadLine()!); 
+if (a % b == 0)(Console.WriteLine("да"));}
+else (Console.WriteLine("нет" + a % b));}
---
+++ lesson2-2/2_4/Program.cs
@@ -0,0 +1,4 @@
+﻿int a = int.Parse(Console.ReadLine()!); 
+
+if (a<100) Console.WriteLine("нет");
+else Console.WriteLine("да " + a / 100 % 10);

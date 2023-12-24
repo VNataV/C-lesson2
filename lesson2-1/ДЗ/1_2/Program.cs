9b23da2f5e77d7eda7ca0e054b40a7608ee081cd
Natalja Veshcheva <rod2vek@gmail.com>
2023-12-24 20:58:45 +0300

    неудачное дз2

---
+++ lesson2-1/ДЗ/1_2/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+if (a < b)
+  {max = b};
+else if (max < c)
+  {max = c};
+Console.WriteLine({max});

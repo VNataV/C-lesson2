2fbafd240dab2639cc654631816f1ed20d5ea954
Natalja Veshcheva <rod2vek@gmail.com>
2023-12-26 21:39:12 +0300

    save

--- lesson2-1/ДЗ/1_2/Program.cs
+++ lesson2-1/ДЗ/1_2/Program.cs
@@ -1,8 +1,6 @@
 ﻿int a = int.Parse(Console.ReadLine()!);
 int b = int.Parse(Console.ReadLine()!);
 int c = int.Parse(Console.ReadLine()!);
-if (a < b)
-  {max = b};
-else if (max < c)
-  {max = c};
-Console.WriteLine({max});
+    if (a > b && a > c) return a;
+      else if (b > c && b > a) return b;
+      else return c;

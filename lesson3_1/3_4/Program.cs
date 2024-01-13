9e68b13246ff534b719b688adf44e1fe84340dac
Natalja Veshcheva <rod2vek@gmail.com>
2024-01-13 22:56:48 +0300

    два задания в одном файле

---
+++ lesson3_1/3_4/Program.cs
@@ -0,0 +1,30 @@
+﻿Random rnd = new Random();
+
+int size = 1000;
+int [] arr_int = new int[size];
+int i = 0;
+
+while(i < size)
+{
+    arr_int[i] = rnd.Next(1, 10);
+    i++;
+}
+i = 0;
+while(i < size)
+{
+    Console.Write($"{arr_int[i]} ");
+    i++;
+}
+
+
+
+int size = 10;
+int [] arr_int = new int[size];
+int i = 0;
+while(i < size)
+{
+    Console.WriteLine("Введите целое число: ");
+    string input = Console.ReadLine();
+    arr_int[i] = Convert.ToInt32(input);
+    i++;
+}

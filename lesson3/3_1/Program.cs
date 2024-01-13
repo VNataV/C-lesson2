310aa6022459cd5bc15127a5bb4c8cf6e032c2a8
Natalja Veshcheva <rod2vek@gmail.com>
2024-01-13 21:01:49 +0300

    new lesson3

---
+++ lesson3/3_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] a1 = { 1, 5, 8, 15, 7 };
+int a2 = int.Parse(Console.ReadLine()!);
+bool b = false;
+
+for (int i = 0; i < a1.Length; i++)
+{
+    if (a2 == a1[i])
+     {b = true;
+     break;
+     }
+}
+if (b)
+    Console.Write("да");
+else
+    Console.Write("нет");
---
+++ lesson3/3_2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+int[] array = { 1, 4, 8, -5, 9, 10, -3, 8, -5, -1, 7 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * -1;
+    Console.Write($"{array[i]} ");
+}

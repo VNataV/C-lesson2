4e87f4ef65b4fd7c2da5b3a7068536776c26a430
Natalja Veshcheva <rod2vek@gmail.com>
2024-01-14 20:19:06 +0300

    4_1от Виктора

---
+++ lesson4/4_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+int[] numbers = new int [10];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(1, 100);
+}
+int count = 0;
+foreach (var number in numbers)
+{
+    if (IsPrime(number))
+    {
+        Console.WriteLine(number);
+        count++;
+    }
+}
+Console.WriteLine("Количество простых чисел в массиве: " + count);
+static bool IsPrime(int number) 
+{
+    if (number < 2) return false;
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+return true;
+}

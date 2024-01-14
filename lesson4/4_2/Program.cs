587cb2a29e7877ef545a1c307baaa23258b27d30
Natalja Veshcheva <rod2vek@gmail.com>
2024-01-14 20:46:39 +0300

    4_2 от Ивана

---
+++ lesson4/4_2/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+int myCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 7 == 0 && arr[i] % 10 == 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int result = myCount(mass);
+Console.WriteLine(result);
